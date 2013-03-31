482399810d0be0ab54c7f7b433f19643693a9d69
rihardsk <rihards@advanced-script-development.com>
2013-03-31 15:36:50 +0300

    Added run button

--- Scripts/cpu-time.js
+++ Scripts/cpu-time.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function () {
+	$("#runButton").click(function () {
+		var scheduler = new fcfs();
+		var text = $("#input").val();
+		var speed = $("#speed").val();
+		scheduler.Initialize(text);
+		scheduler.Start(speed);
+	});
+});
